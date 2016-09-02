221921cbe8841fb0a93ee349f5a6b4298cb8d24b
Eric VanDever <evandever@blackpointcyber.com>
2016-09-02 09:07:22 -0400

    selectAllUnfiltered (things that pass the filter)

--- src/ts/gridApi.ts
+++ src/ts/gridApi.ts
@@ -367,6 +367,10 @@ export class GridApi {
         this.selectionController.selectAllRowNodes();
     }
 
+    public selectAllUnfiltered() {
+        this.selectionController.selectAllUnfilteredRowNodes();
+    }
+
     public deselectAll() {
         this.selectionController.deselectAllRowNodes();
     }
